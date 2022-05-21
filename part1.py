59897a1a63ef65e9600c1786bf91b763d664da51
Marlena Sewerynek <marlena_duda@onet.eu>
2022-05-21 17:04:33 +0200

    Added 2 py files with schema and exercises for part1 of training

---
+++ part1.py
@@ -0,0 +1,17 @@
+# importy
+
+# Tworzenie Series
+
+# Tworzenie DataFrame
+
+# Wyswietlanie danych
+
+# Dodawanie kolumn
+
+# Usuwanie kolumn
+
+# Filtrowanie danych
+
+# Operacje na Series i DataFrames
+
+# Łączenie DataFrames
---
+++ part1_exercises.py
@@ -0,0 +1,50 @@
+"""
+Zadanie 1:
+Stwórz dataframe z dzięsięcioma imionami uczniów i uczennic oraz liczbą punktów, jakie uzyskali z egzaminu.
+Następnie sprawdź, jaka była średnia i mediana wyników.
+"""
+
+"""
+Zadanie 2: 
+Bazując na ramce danych utworzonej w poprzednim zadaniu, wyświetl czwarty wiersz. Następnie zapisz do osobnej ramki 
+tych uczniów, którzy uzyskali wynik powyżej średniej.
+"""
+
+
+"""
+Zadanie 3:
+Dodaj do ramki trzecią kolumnę, w której znajdzie się procentowy wynik uczniów i uczennic z tego egzaminu 
+(to znaczy, że każda wartość liczbowa musi zostać podzielona przez maksimum punktów, jakie można było na tym 
+egzaminie uzyskać).
+Następnie, chcemy zanonimizować kolumnę z imionami: chcemy, by została tylko pierwsza i ostatnia litera imienia. 
+Np. dla imienia “Marcelina” chcemy zachować “M...a”
+"""
+
+
+"""
+Zadanie 4:
+Posortuj ramkę danych względem liczby punktów otrzymanych z egzaminu, od najwyższego do najniższego. 
+W przypadku takiej samej liczby punktów, osoby mają zostać wyświetlone w kolejności alfabetycznej.
+"""
+
+
+"""
+Zadanie 5:
+Połącz ze sobą dwie poniższe ramki.
+
+student_data1 = pd.DataFrame({
+'student_id': ['S1', 'S2', 'S3', 'S4', 'S5'],
+'name': ['Danniella Fenton', 'Ryder Storey', 'Bryce Jensen', 'Ed Bernal', 'Kwame Morin'] })
+
+student_data2 = pd.DataFrame({
+'student_id': ['S6', 'S7', 'S8', 'S9', 'S10'],
+'name': ['Scarlette Fisher', 'Carla Williamson', 'Dante Morse', 'Kaiser William', 'Madeeha Preston'] })
+"""
+
+"""
+Zadanie 6:
+exam_results = pd.DataFrame({
+'student_id': ['S2', 'S10', 'S3', 'S1', 'S7', 'S9', 'S5', 'S4', 'S8', 'S6'], 
+'marks': [200, 210, 190, 222, 199, 201, 200, 198, 219, 201]
+})
+"""
