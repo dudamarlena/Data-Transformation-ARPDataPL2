97dd5569059c35380eb2f9272731117ed24049c8
Marlena Sewerynek <marlena_duda@onet.eu>
2022-05-22 11:35:24 +0200

    agg functions

--- part1.py
+++ part1.py
@@ -60,10 +60,13 @@ print(new_df['col3'] == "A")
 print(a_values)
 
 a_4_values = new_df[(new_df['col3'] == "A") & (new_df['col1'] > 3)]
-print(a_4_values)
 
 a_values['col2'][a_values['col2'] > 3] = 0
-print(a_values)
+
+print(f'Mean: {new_df["col1"].mean()}, Std: {new_df["col1"].std()}, Min: {new_df["col1"].min()}, '
+      f'Count: {new_df["col1"].count()} or {len(new_df["col1"])}')
+
+
 
 # Operacje na Series i DataFrames
 
