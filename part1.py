97d7390a8f8d6869cc0acf8031a1301813fdc1f9
Marlena Sewerynek <marlena_duda@onet.eu>
2022-05-22 09:52:22 +0200

    adda series

--- part1.py
+++ part1.py
@@ -1,7 +1,9 @@
 # import
 import pandas as pd
+import numpy as np
 
 # Tworzenie Series
+s1 = pd.Series([1, 2, 3, 4], index=['A','B', 'C', 'D'], name='numbers', dtype=int)
 
 # Tworzenie DataFrame
 
