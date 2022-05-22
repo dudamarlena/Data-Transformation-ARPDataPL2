b2549c09575ada96746a4436ddd5879542859d71
Marlena Sewerynek <marlena_duda@onet.eu>
2022-05-22 15:39:15 +0200

    read data

--- part2.py
+++ part2.py
@@ -1,1 +1,17 @@
 import pandas as pd
+
+data = pd.read_csv(r"Datasets/cars.csv", sep=";")
+print(data.info())
+print(data.describe())
+# print(data.head(n=10))
+# print(data.tail(n=10))
+# print(data.sample(n=10))
+
+objects_data = data.select_dtypes(object)
+print(objects_data.describe())
+
+float_data = data.select_dtypes(float)
+print(float_data.describe())
+
+int_data = data.select_dtypes(int)
+print(int_data.describe())
