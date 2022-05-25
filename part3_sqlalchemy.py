d3bdc0ed45cda3e3bf1fce06d00989871dacfc7d
Marlena Sewerynek <marlena_duda@onet.eu>
2022-05-25 20:45:44 +0200

    sqlalchemy2

--- part3_sqlalchemy.py
+++ part3_sqlalchemy.py
@@ -5,7 +5,22 @@ from sqlalchemy import Column, String, Integer
 from sqlalchemy.orm import sessionmaker
 import pandas as pd
 
-eng = create_engine(f"mysql+pymysql://root:{password}@localhost/car_rental")
+engine = create_engine(f"mysql+pymysql://root:{password}@localhost/car_rental")
 
-tables = eng.execute("SHOW TABLES")
-print(list(tables))
+tables = engine.execute("SHOW TABLES")
+
+columns = engine.execute("SHOW COLUMNS FROM clients;")
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), default='Unknown')
+
+
+# Base.metadata.create_all(engine)
+
+print(list(engine.execute("SHOW TABLES")))
